601e01e81eb1d426f616a29c23aac19efa8d8128
unknown <businessbhoumic@gmail.com>
2025-10-02 22:27:17 +0530

    Added contact, footer, teams sections
     modifications are planned, sample designs had been added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ AOS.init({
   offset: 50,
   easing: "ease-out-cubic"
 });
+
+const menuBtn = document.getElementById("menu");
+const sidebar = document.querySelector(".sidebar");
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
